3ad70772f75331c46fabcdb69894522f904caf24
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-06-17 15:16:59 -0300

    S5-31 Agrego clase principal de la API

---
+++ src/main/java/sistemaGestion/GestionRecursosRestApplication.java
@@ -0,0 +1,13 @@
+package sistemaGestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionRecursosRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionRecursosRestApplication.class, args);
+    }
+
+}
