4035db7e85dc17c1c2d973ad86f3410a12462a6b
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-06-22 17:57:14 -0300

    SDIG-58 Muevo Definicion de clase SpringIntegrationtests para que hereden la configuracion las clases de tests

---
+++ src/test/java/com/sistemaGestion/SpringIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.sistemaGestion;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration(classes = GestionRecursosRestApplication.class)
+@CucumberContextConfiguration
+@SpringBootTest
+public class SpringIntegrationTest {
+
+}
