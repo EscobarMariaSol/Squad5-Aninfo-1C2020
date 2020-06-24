b07c10fcb7fad1b10a0a6a86e3df0f6110b1e9f9
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-06-24 15:47:56 -0300

    SIDG-59 Implementacion de método que obtiene un empleado por legajo en el servicio

--- src/main/java/com/sistemaGestion/service/EmpleadoService.java
+++ src/main/java/com/sistemaGestion/service/EmpleadoService.java
@@ -1,11 +1,13 @@
 package com.sistemaGestion.service;
 
+import com.sistemaGestion.exceptions.EmpleadoException;
 import com.sistemaGestion.repository.EmpleadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.sistemaGestion.model.Empleado;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmpleadoService {
@@ -19,6 +21,13 @@ public class EmpleadoService {
 
     public List<Empleado> consultarEmpleados() {
         return empleadoRepository.findAll();
+    }
+
+    public Empleado consultarEmpleadoPorLegajo(String legajo) {
+        return empleadoRepository.findByLegajo(legajo)
+                .orElseThrow( () ->
+                        new EmpleadoException("Empleado with legajo " + legajo + " not found.")
+                );
     }
 
 }
