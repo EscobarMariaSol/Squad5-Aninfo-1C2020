e705e64fe4b9ff3bf04de0cfa70afa4d674eadaa
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-06-29 17:32:22 -0300

    SIDG-61 Declaracion de metodos para obtener todos los empleados con el parametro activo en true y obtencion de un empleado segun legajo con el parametro activo true

--- src/main/java/com/sistemaGestion/repository/EmpleadoRepository.java
+++ src/main/java/com/sistemaGestion/repository/EmpleadoRepository.java
@@ -4,6 +4,7 @@ import com.sistemaGestion.model.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +13,8 @@ public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
     Optional<Empleado> findByLegajo(String legajo);
 
     Optional<Empleado> findById(Long id);
+
+    Optional<Empleado> findByLegajoAndActivoIsTrue(String legajo);
+
+    List<Empleado> findAllByActivoIsTrue();
 }
