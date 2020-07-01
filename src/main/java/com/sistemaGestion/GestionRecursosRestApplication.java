ccfcab058858dadc984720e51fc3bd26a54625ac
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-07-01 18:44:01 -0300

    SIDG-61 Agrego configuracion de CORS para poder comunicarnos con el front

--- src/main/java/com/sistemaGestion/GestionRecursosRestApplication.java
+++ src/main/java/com/sistemaGestion/GestionRecursosRestApplication.java
@@ -2,12 +2,28 @@ package com.sistemaGestion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class GestionRecursosRestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GestionRecursosRestApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://mapache-web.herokuapp.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
     }
 
 }
