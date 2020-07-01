609005246ac238412c695dc28dd55f90d38affe3
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-07-01 11:40:34 -0300

    SIDG-61 indexo tarea con TareaId y agrego asociacion con empleados

--- src/main/java/com/sistemaGestion/model/Tarea.java
+++ src/main/java/com/sistemaGestion/model/Tarea.java
@@ -5,17 +5,10 @@ import javax.persistence.*;
 @Entity
 public class Tarea {
 
-    @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Long id;
-
-    /**
-     * Código identificador de la tarea que viene del Módulo de Proyectos.
-     * */
-    @Column
-    private String codigo;
+    @EmbeddedId
+    private TareaId id;
 
     @ManyToOne
-    private Proyecto proyecto;
+    private Empleado empleado;
 
 }
