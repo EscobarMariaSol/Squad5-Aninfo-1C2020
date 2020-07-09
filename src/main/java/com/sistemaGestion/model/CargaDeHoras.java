a404cc16dbced01508a70b06ac4e7ee822441bcd
JulietaCaceres <julieta.agustina.caceres@gmail.com>
2020-07-09 16:53:24 -0300

    SIDG-4 Borrando lombock dependency

--- src/main/java/com/sistemaGestion/model/CargaDeHoras.java
+++ src/main/java/com/sistemaGestion/model/CargaDeHoras.java
@@ -1,13 +1,11 @@
 package com.sistemaGestion.model;
 
-import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
-@Data
 public class CargaDeHoras {
 
     @Id @GeneratedValue(generator="system-uuid")
@@ -20,11 +18,51 @@ public class CargaDeHoras {
     @Column
     public String proyectoId;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTareaId() {
+        return tareaId;
+    }
+
+    public void setTareaId(String tareaId) {
+        this.tareaId = tareaId;
+    }
+
+    public String getProyectoId() {
+        return proyectoId;
+    }
+
+    public void setProyectoId(String proyectoId) {
+        this.proyectoId = proyectoId;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public Integer getHorasTrabajadas() {
+        return horasTrabajadas;
+    }
+
+    public void setHorasTrabajadas(Integer horasTrabajadas) {
+        this.horasTrabajadas = horasTrabajadas;
+    }
+
     @Column
     public LocalDate fecha;
 
     @Column
-    public int horasTrabajadas;
+    public Integer horasTrabajadas;
 
     public CargaDeHoras() {
 
@@ -35,5 +73,33 @@ public class CargaDeHoras {
         this.fecha =fecha;
         this.horasTrabajadas = horas;
     }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "tareaId: " + tareaId +
+                        ", proyectoId: " + proyectoId +
+                        ", fecha: " + fecha.toString() +
+                        ", horasCargadas: " + horasTrabajadas
+        );
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        CargaDeHoras cargaDeHoras = (CargaDeHoras) o;
+        return proyectoId.equals(cargaDeHoras.getProyectoId()) &&
+                fecha.equals(cargaDeHoras.getFecha()) &&
+                horasTrabajadas.equals(cargaDeHoras.getHorasTrabajadas()) &&
+                tareaId.equals(cargaDeHoras.getTareaId());
+    }
+
+
 
 }
