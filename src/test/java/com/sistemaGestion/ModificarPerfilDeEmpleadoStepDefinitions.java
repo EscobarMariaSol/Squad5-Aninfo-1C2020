e9559a2f410f3cad06c6b481adb34451ff22664a
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-07-13 18:14:25 -0300

    SIDG-56 Modifico tests para usar nueva definicion del endpoint

--- src/test/java/com/sistemaGestion/ModificarPerfilDeEmpleadoStepDefinitions.java
+++ src/test/java/com/sistemaGestion/ModificarPerfilDeEmpleadoStepDefinitions.java
@@ -2,8 +2,10 @@ package com.sistemaGestion;
 
 import com.sistemaGestion.assets.EmpleadoFactory;
 import com.sistemaGestion.controller.EmpleadoController;
+import com.sistemaGestion.dtos.PerfilEmpleadoDTO;
 import com.sistemaGestion.exceptions.EmpleadoException;
 import com.sistemaGestion.model.Empleado;
+import com.sistemaGestion.repository.EmpleadoRepository;
 import com.sistemaGestion.service.EmpleadoService;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.es.Cuando;
@@ -20,30 +22,42 @@ import java.util.Map;
 public class ModificarPerfilDeEmpleadoStepDefinitions extends SpringIntegrationTest {
 
     @Autowired
-    private EmpleadoService empleadoService;
+    private EmpleadoRepository empleadoRepository;
 
     @Autowired
     private EmpleadoController empleadoController;
 
+    private PerfilEmpleadoDTO perfilEmpleadoDTO;
     private Empleado empleado;
     private ResponseEntity response;
 
     @Y("existe el  empleado con los atributos")
     public void existeElEmpleadoConLosAtributos(DataTable empleadoDt) {
         List<Map<String, String>> empleados = empleadoDt.asMaps(String.class, String.class);
+        perfilEmpleadoDTO = EmpleadoFactory.crearPerfilEmpleadoDTO(empleados.get(0));
         empleado = EmpleadoFactory.crearEmpleado(empleados.get(0));
         empleadoController.ingresarEmpleado(empleado);
     }
 
     @Cuando("cambio el nombre del empleado por {string}")
-    public void cambioElNombreDelEmpleadoPorRowena(String nuevoNombre) {
-        empleado.setNombre(nuevoNombre);
-        response = empleadoController.actualizarEmpleado(empleado);
+    public void cambioElNombreDelEmpleadoPor(String nuevoNombre) {
+        perfilEmpleadoDTO = new PerfilEmpleadoDTO.Builder().con(empleadoData -> {
+            empleadoData.conNombre(nuevoNombre);
+            empleadoData.conApellido(perfilEmpleadoDTO.getApellido());
+            empleadoData.conDni(perfilEmpleadoDTO.getDni());
+            empleadoData.conFechaNacimiento(perfilEmpleadoDTO.getFechaNacimiento());
+            empleadoData.conLegajo(perfilEmpleadoDTO.getLegajo());
+            empleadoData.conRol(perfilEmpleadoDTO.getRol());
+            empleadoData.conContrato(perfilEmpleadoDTO.getContrato());
+            empleadoData.conSeniority(perfilEmpleadoDTO.getSeniority());
+            empleadoData.conActivo(perfilEmpleadoDTO.getActivo());
+        }).build();
+        response = empleadoController.actualizarEmpleado(perfilEmpleadoDTO);
     }
 
     @Entonces("el empleado con legajo {string} presenta los siguientes datos")
     public void elEmpleadoConLegajoPresentaLosSiguientesDatos(String legajo, DataTable empleadoDt) {
-        Empleado empleadoActualizado = empleadoService.consultarEmpleadoPorLegajo(legajo);
+        Empleado empleadoActualizado = empleadoRepository.findByLegajo(legajo).orElse(null);
 
         List<Map<String, String>> empleados = empleadoDt.asMaps(String.class, String.class);
         Empleado empleadoEsperado = EmpleadoFactory.crearEmpleado(empleados.get(0));
