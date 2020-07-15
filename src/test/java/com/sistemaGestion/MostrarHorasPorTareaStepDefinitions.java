3e5e7499e3783811915fa0749e320eece2271151
EscobarMariaSol <mariasolescobar21@gmail.com>
2020-07-15 14:03:58 -0300

    comienzo actualizando bdd

--- src/test/java/com/sistemaGestion/MostrarHorasPorTareaStepDefinitions.java
+++ src/test/java/com/sistemaGestion/MostrarHorasPorTareaStepDefinitions.java
@@ -1,22 +1,62 @@
 package com.sistemaGestion;
 
 import com.sistemaGestion.assets.EmpleadoFactory;
+import com.sistemaGestion.controller.EmpleadoController;
 import com.sistemaGestion.model.Empleado;
+import com.sistemaGestion.model.Seniority;
+import com.sistemaGestion.repository.EmpleadoRepository;
+import io.cucumber.java.After;
 import io.cucumber.java.es.Cuando;
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.es.Entonces;
 import io.cucumber.java.es.Y;
-
-import java.util.ArrayList;
+import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 public class MostrarHorasPorTareaStepDefinitions {
 
+    @Autowired
+    private EmpleadoRepository empleadoRepository;
+
+    @Autowired
+    private EmpleadoController empleadoController;
+
+    private Empleado liderDeRecursosHumanos, empleado;
+    private ResponseEntity response;
+
     @Dado("^que soy el lider de recursos humanos$")
     public void soy_lider_de_recursos_humanos() {
+        liderDeRecursosHumanos = EmpleadoFactory.crearLiderDeRecursosHumanos();
     }
 
-    @Y("hay {int} empleado registrado")
-    public void hay_empleado_registrado(Integer int1) {
+
+    @Cuando("consulto las horas trabajadas por el empleado con legajo {string},en la tarea {string} del proyecto {string}, para la fecha {string}")
+    public void consulto_las_horas_trabajadas_por_el_empleado_con_legajo_en_la_tarea_del_proyecto_para_la_fecha(String legajo,
+                                                                                                                String idTarea,
+                                                                                                                String idProyecto,
+                                                                                                                String fecha) {
+        // Write code here that turns the phrase above into concrete actions
+        //response = empleadoController.consultarHorasEnUnaTarea(legajo, idTarea, idProyecto, fecha);
+    }
+
+    @Entonces("obtengo la información de la cantidad de horas trabajadas por el empleado en la tarea indicada durante la fecha indicada.")
+    public void obtengo_la_información_de_la_cantidad_de_horas_trabajadas_por_el_empleado_en_la_tarea_indicada_durante_la_fecha_indicada() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @Cuando("consulto las horas trabajadas por el empleado con legajo {string},en la tarea {string} del proyecto {string}")
+    public void consulto_las_horas_trabajadas_por_el_empleado_con_legajo_en_la_tarea_del_proyecto(String string, String string2, String string3) {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @Entonces("obtengo la información de la cantidad de horas trabajadas por el empleado en la tarea indicada.")
+    public void obtengo_la_información_de_la_cantidad_de_horas_trabajadas_por_el_empleado_en_la_tarea_indicada() {
+        // Write code here that turns the phrase above into concrete actions
+
     }
 
     @Y("no hay empleados registrados")
@@ -24,24 +64,14 @@ public class MostrarHorasPorTareaStepDefinitions {
         // Write code here that turns the phrase above into concrete actions
     }
 
-    @Cuando("realizo una búsqueda de un empleado en el sistema")
-    public void realizo_una_búsqueda_de_un_empleado_en_el_sistema() {
+    @Entonces("se me informa que no puedo realizar dicha acción ya que el emplado con legajo {string} no existe.")
+    public void se_me_informa_que_no_puedo_realizar_dicha_acción_ya_que_el_emplado_con_legajo_no_existe(String string) {
         // Write code here that turns the phrase above into concrete actions
+
     }
 
-    @Y("selecciono sus tareas asignadas")
-    public void selecciono_sus_tareas_asignadas() {
-        // Write code here that turns the phrase above into concrete actions
-    }
-
-
-    @Entonces("obtengo la información detallada de la cantidad de horas trabajadas por el empleado en cada tarea.")
-    public void obtengo_la_información_detallada_de_la_cantidad_de_horas_trabajadas_por_el_empleado_en_cada_tarea() {
-        // Write code here that turns the phrase above into concrete actions
-    }
-
-    @Entonces("obtengo un mensaje indicandome que no hay empleados registrados")
-    public void obtengo_un_mensaje_indicandome_que_no_hay_empleados_registrados() {
-        // Write code here that turns the phrase above into concrete actions
+    @After
+    public void tearDown() {
+        empleadoRepository.deleteAll();
     }
 }
--- src/test/resources/com/sistemaGestion/mostrar-horas-por-tarea.feature
+++ src/test/resources/com/sistemaGestion/mostrar-horas-por-tarea.feature
@@ -5,15 +5,25 @@ Característica: mostrar horas trabajadas por un empleado en una tarea
   Escenario: como lider de recursos humanos quiero consultar las horas trabajadas por un empleado en una tarea,
     para poder liquidar su sueldo.
     Dado que soy el lider de recursos humanos
-    Y hay 1 empleado registrado
-    Cuando realizo una búsqueda de un empleado en el sistema
-    Y selecciono sus tareas asignadas
-    Entonces obtengo la información detallada de la cantidad de horas trabajadas por el empleado en cada tarea.
+    Y existe el empleado con los atributos
+      | nombre      | apellido   | dni      | fechaDeNacimiento | legajo | contrato      | rol           | activo |
+      | Hermione    | Granger    | W3508429 |1979-09-19         | 123    |  full-time    | DESARROLLADOR | true   |
+    Cuando consulto las horas trabajadas por el empleado con legajo '123',en la tarea '1' del proyecto '1', para la fecha '2020-04-03'
+    Entonces obtengo la información de la cantidad de horas trabajadas por el empleado en la tarea indicada durante la fecha indicada.
+
+  Escenario: como lider de recursos humanos quiero consultar las horas trabajadas por un empleado en una tarea,
+  para poder liquidar su sueldo.
+    Dado que soy el lider de recursos humanos
+    Y existe el empleado con los atributos
+      | nombre      | apellido   | dni      | fechaDeNacimiento | legajo | contrato      | rol           | activo |
+      | Hermione    | Granger    | W3508429 |1979-09-19         | 123    |  full-time    | DESARROLLADOR | true   |
+    Cuando consulto las horas trabajadas por el empleado con legajo '123',en la tarea '1' del proyecto '1'
+    Entonces obtengo la información de la cantidad de horas trabajadas por el empleado en la tarea indicada.
 
 
   Escenario: como lider de recursos humanos quiero consultar las horas trabajadas por un empleado en una tarea,
   para poder liquidar su sueldo.
     Dado que soy el lider de recursos humanos
     Y no hay empleados registrados
-    Cuando realizo una búsqueda de un empleado en el sistema
-    Entonces obtengo un mensaje indicandome que no hay empleados registrados
+    Cuando consulto las horas trabajadas por el empleado con legajo '123',en la tarea '1' del proyecto '1'
+    Entonces se me informa que no puedo realizar dicha acción ya que el emplado con legajo '123' no existe.
