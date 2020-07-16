d578b5ea3fb2f021a1ab63f43a1cf8aebe47a77a
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-07-15 21:10:05 -0300

    SIDG-56 Definicion del enum de Contrato de empleados

---
+++ src/main/java/com/sistemaGestion/model/enums/EmpleadoContrato.java
@@ -0,0 +1,16 @@
+package com.sistemaGestion.model.enums;
+
+public enum EmpleadoContrato {
+
+
+    /**
+     * Contrato Part-time: 30hs semanales
+     * */
+    PART_TIME,
+
+    /**
+     * Contrato Full-Time: 40hs semanales
+     * */
+    FULL_TIME
+
+}
