c50671c91516a78efb9eb2b9669b01788cbcb6f4
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-07-28 23:29:15 -0300

    SIDG-70 Se agrega el enum de Actividad

---
+++ src/main/java/com/sistemaGestion/model/enums/Actividad.java
@@ -0,0 +1,20 @@
+package com.sistemaGestion.model.enums;
+
+public enum Actividad {
+
+    /**
+     * La actividad es una tarea y como tal pertenece a un proyecto
+     * */
+    TAREA,
+
+    /**
+     * La actividad es de vacaciones
+     * */
+    VACACIONES,
+
+    /**
+     * La actividad es de enfermedad
+     * */
+    ENFERMEDAD
+
+}
