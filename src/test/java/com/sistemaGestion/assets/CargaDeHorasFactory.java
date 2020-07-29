0c0f214cdc907eefc3f1046481432c3e9d4e389c
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-07-28 23:48:40 -0300

    SIDG-70 Se agrega parentesis faltante

--- src/test/java/com/sistemaGestion/assets/CargaDeHorasFactory.java
+++ src/test/java/com/sistemaGestion/assets/CargaDeHorasFactory.java
@@ -14,7 +14,7 @@ public class CargaDeHorasFactory {
         Long codigoTarea = Long.parseLong(atributosCargaDeHoras.get("codigoTarea"));
         Long codigoProyecto = Long.parseLong(atributosCargaDeHoras.get("codigoProyecto"));
         String legajo = atributosCargaDeHoras.get("legajo");
-        LocalDate fecha = LocalDate.parse(atributosCargaDeHoras.get("fecha");
+        LocalDate fecha = LocalDate.parse(atributosCargaDeHoras.get("fecha"));
         Float cantidadHoras = Float.valueOf(atributosCargaDeHoras.get("cantidadHoras"));
         Actividad actividad = Actividad.valueOf(atributosCargaDeHoras.get("actividad"));
 
